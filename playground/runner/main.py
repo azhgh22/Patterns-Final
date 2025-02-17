ac99d65b4fb43beb7442f0d96ec29331739b7f4d
Archil <azhgh22@freeuni.edu.ge>
2025-02-17 20:53:46 +0400

    setup

--- playground/runner/main.py
+++ playground/runner/main.py
@@ -1,6 +1,6 @@
 from apexdevkit.server import UvicornServer
 
-from playground.runner.setup import setup
+from playground.runner.setup import setup, SetupConfiguration, MemoryType
 
 if __name__ == "__main__":
-    UvicornServer.from_env().run(setup())
+    UvicornServer.from_env().run(setup(SetupConfiguration(MemoryType.IN_MEMORY)))
--- playground/runner/setup.py
+++ playground/runner/setup.py
@@ -1,7 +1,20 @@
+import enum
+from dataclasses import dataclass
+
 from fastapi import FastAPI
 
 
-def setup() -> FastAPI:
+class MemoryType(enum.Enum):
+    IN_MEMORY = 0
+    SQL_LITE = 1
+
+
+@dataclass
+class SetupConfiguration:
+    memory_type: MemoryType
+
+
+def setup(setup_conf: SetupConfiguration) -> FastAPI:
     api = FastAPI()
 
     return api
