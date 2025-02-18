baef21c4ad26f9ad9650ed4789aedfca10d96761
5w7tch <nurch22@freeuni.edu.ge>
2025-02-18 22:44:40 +0400

    endpoints inited

--- playground/infra/API/products_api.py
+++ playground/infra/API/products_api.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter
 
 from playground.core.models.product import Product, ProductRequest
@@ -7,4 +9,12 @@ products_api = APIRouter()
 
 @products_api.post("")
 async def create_product(product: ProductRequest) -> Product:
+    pass
+
+@products_api.get("")
+async def get_products() -> List[Product]:
+    pass
+
+@products_api.patch("/{product_id}")
+async def update_product(product_id: str) -> Product:
     pass
--- playground/runner/setup.py
+++ playground/runner/setup.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 from fastapi import FastAPI
 
 from playground.infra.API.products_api import products_api
+from playground.infra.API.report_api import sales_api, x_reports_api
 
 
 class MemoryType(enum.Enum):
@@ -17,6 +18,8 @@ class SetupConfiguration:
 
 def set_up_routes(api: FastAPI) -> None:
     api.include_router(products_api, prefix="/products", tags=["Products"])
+    api.include_router(sales_api, prefix="/sales", tags=["sales"])
+    api.include_router(x_reports_api, prefix="/x-reports", tags=["x_reports"])
 
 
 def setup(setup_conf: SetupConfiguration) -> FastAPI:
