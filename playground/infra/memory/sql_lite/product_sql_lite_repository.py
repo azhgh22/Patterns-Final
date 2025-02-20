8e1cbb4b352a8eb18af4a7a2100448adccf46ebc
Archil <azhgh22@freeuni.edu.ge>
2025-02-20 20:57:29 +0400

    finished product sql_lite part

--- playground/infra/memory/sql_lite/product_sql_lite_repository.py
+++ playground/infra/memory/sql_lite/product_sql_lite_repository.py
@@ -1,24 +1,54 @@
 import sqlite3
-from dataclasses import dataclass
 
 from playground.core.models.product import Product
 
 
-@dataclass
 class ProductSqlLiteRepository:
-    connection: sqlite3.Connection
+    def __init__(self, connection: sqlite3.Connection):
+        self.conn = connection
+        self.__create_table()
+
+    def __create_table(self) -> None:
+        self.conn.execute("""
+                    create table if not exists products (
+                        id Text,
+                        product_name Text,
+                        barcode Text,
+                        price Integer
+                    )
+                """)
+        self.conn.commit()
+
+    def __get_product(self, key: str, value: str) -> list[Product]:
+        raw = self.conn.execute(f"""
+                    select * from products where {key} = '{value}'
+                """).fetchall()
+        return list(Product(x[0], x[1], x[2], x[3]) for x in raw)
 
     def get_product_with_id(self, p_id: str) -> Product | None:
-        pass
+        p_list = self.__get_product("id", p_id)
+        return p_list[0] if len(p_list) > 0 else None
 
     def get_all_products(self) -> list[Product]:
-        pass
+        return self.__get_product('"1"', "1")
 
     def update_price(self, p_id: str, price: int) -> bool:
-        pass
+        updated_rows = self.conn.execute(f"""
+                    update products
+                    set price={price}
+                    where id = {p_id}
+                """).rowcount
+        return updated_rows == 1
 
-    def store_product(self, prod: Product) -> None:
-        pass
+    def store_product(self, product: Product) -> None:
+        self.conn.execute(
+            """
+        insert into products (id, product_name, barcode, price)
+        values (?,?,?,?)
+         """,
+            (product.id, product.name, product.barcode, product.price),
+        )
+        self.conn.commit()
 
     def contains_product_with_barcode(self, barcode: str) -> bool:
-        pass
+        return len(self.__get_product("barcode", barcode)) != 0
