b937d2277cd2c44166bb02983637b4a5969e156f
Lasha Chikhradze <lchikhradze@velitech.com>
2025-02-22 16:01:53 +0400

    add shift service to service chooser class

--- playground/core/services/classes/service_chooser.py
+++ playground/core/services/classes/service_chooser.py
@@ -1,12 +1,19 @@
 from playground.core.services.classes.product_service import ProductService
+from playground.core.services.interfaces.memory import ShiftRepository
 from playground.core.services.interfaces.memory.product_repository import (
     ProductRepository,
 )
 from playground.core.services.interfaces.service_interfaces.product_service_interface import (
     IProductService,
 )
+from playground.core.services.interfaces.service_interfaces.shift_service_interface import (
+    IShiftService,
+)
 
 
 class ServiceChooser:
     def get_product_service(self, product_repo: ProductRepository) -> IProductService:
         return ProductService(product_repo)
+
+    def get_shift_service(self, shift_repo: ShiftRepository) -> IShiftService:
+        pass
