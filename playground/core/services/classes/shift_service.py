59adea865056ceacf5494a29e40b0cd4f654662a
Lasha Chikhradze <lchikhradze@velitech.com>
2025-02-22 20:58:01 +0400

    add import for receipt service in the shift service

--- playground/core/services/classes/shift_service.py
+++ playground/core/services/classes/shift_service.py
@@ -7,6 +7,7 @@ from playground.core.models.product import ProductReport
 from playground.core.models.revenue import Revenue
 from playground.core.models.shift import Shift
 from playground.core.models.x_report import XReport
+from playground.core.services.classes.receipt_service import ReceiptService
 from playground.core.services.interfaces.memory.shift_repository import ShiftRepository
 from playground.infra.memory.in_memory.shift_in_memory_repository import (
     ShiftInMemoryRepository,
@@ -24,12 +25,12 @@ class ShiftService:
         self.repo.store(shift)
         return shift
 
-    def close(self, shift_id: str, receiptService: ReceiptService) -> bool:
+    def close(self, shift_id: str, receipt_service: ReceiptService) -> bool:
         if shift_id != self.get_open_shift_id():
             raise IndexError
         receipt_ids = self.repo.get_shift_receipt_ids(shift_id)
         for receipt_id in receipt_ids:
-            receipt = receiptService.get(receipt_id)
+            receipt = receipt_service.get(receipt_id)
             # TODO: change it to use enum
             if receipt.status == "open":
                 return False
@@ -42,8 +43,8 @@ class ShiftService:
     def get_x_report(
         self,
         shift_id: str,
-        receiptService: ReceiptService,
-        paymentService: PaymentService,
+        receipt_service: ReceiptService,
+        payment_service: PaymentService,
     ) -> XReport:
         items = defaultdict(int)
         sales = defaultdict(int)
@@ -51,30 +52,26 @@ class ShiftService:
         shift_receipt_ids = self.repo.get_shift_receipt_ids(shift_id)
 
         for receipt_id in shift_receipt_ids:
-            receipt = receiptService.get(receipt_id)
-            for product in receipt.products:
-                items[product.product_id] += product.amount
+            receipt = receipt_service.get(receipt_id)
+            for item in receipt.products:
+                items[item.product_id] += item.total
 
-            payment = paymentService.get(receipt_id)
+            payment = payment_service.get(receipt_id)
             sales[payment.currency_id] += payment.amount
 
-        products = [
-            ProductReport(product_id, amount) for product_id, amount in items.items()
-        ]
-        revenue = [
-            Revenue(currency_id, amount) for currency_id, amount in sales.items()
-        ]
+        products = [ProductReport(product_id, amount) for product_id, amount in items.items()]
+        revenue = [Revenue(currency_id, amount) for currency_id, amount in sales.items()]
 
         return XReport(shift_id, len(shift_receipt_ids), products, revenue)
 
     # IMPORTANT: only closed receipts are added to the shift
     def add_receipt(
-        self, shift_id: str, receipt_id: str, receiptService: ReceiptService
+        self, shift_id: str, receipt_id: str, receipt_service: ReceiptService
     ) -> bool:
         open_shift_id = self.get_open_shift_id()
         if open_shift_id is None or open_shift_id != shift_id:
             raise IndexError
-        if receiptService.get(receipt_id).status == "open":
+        if receipt_service.get(receipt_id).status == "open":
             raise ValueError
 
         self.repo.add_receipt(shift_id, receipt_id)
--- playground/core/services/interfaces/service_interfaces/shift_service_interface.py
+++ playground/core/services/interfaces/service_interfaces/shift_service_interface.py
@@ -3,13 +3,14 @@ from typing import Protocol
 from playground.core.models.receipt import Receipt
 from playground.core.models.shift import Shift
 from playground.core.models.x_report import XReport
+from playground.core.services.classes.receipt_service import ReceiptService
 
 
 class IShiftService(Protocol):
     def open(self) -> Shift:
         pass
 
-    def close(self, shift_id: str, receiptService: ReceiptService) -> bool:
+    def close(self, shift_id: str, receipt_service: ReceiptService) -> bool:
         pass
 
     def get_open_shift_id(self) -> str | None:
@@ -18,12 +19,12 @@ class IShiftService(Protocol):
     def get_x_report(
         self,
         shift_id: str,
-        receiptService: ReceiptService,
-        paymentService: PaymentService,
+        receipt_service: ReceiptService,
+        payment_service: PaymentService,
     ) -> XReport:
         pass
 
     def add_receipt(
-        self, shift_id: str, receipt_id: str, receiptService: ReceiptService
+        self, shift_id: str, receipt_id: str, receipt_service: ReceiptService
     ) -> bool:
         pass
