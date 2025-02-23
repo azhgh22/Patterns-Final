9279ea1622b35c89d335202733367b0274edb020
Archil <azhgh22@freeuni.edu.ge>
2025-02-23 22:06:14 +0400

    added payment tests

--- playground/core/services/classes/payment_service.py
+++ playground/core/services/classes/payment_service.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
-import requests
 
 from playground.core.models.payments import PaymentRequest, Payment
+from playground.core.services.interfaces.currency_converter_interface import ICurrencyConverter
 from playground.core.services.interfaces.memory.payment_repository import PaymentRepository
 from playground.core.services.interfaces.service_interfaces.receipt_service_interface import (
     IReceiptService,
 )
+from playground.infra.currency_converter.er_api_converter import ErApiConverter
 from playground.infra.memory.in_memory.payment_in_memory_repository import (
     PaymentInMemoryRepository,
 )
@@ -14,6 +15,7 @@ from playground.infra.memory.in_memory.payment_in_memory_repository import (
 @dataclass
 class PaymentService:
     repo: PaymentRepository = PaymentInMemoryRepository()
+    converter: ICurrencyConverter = ErApiConverter()
 
     def calculate_payment(
         self, receipt_id: str, currency_id: str, receipt_service: IReceiptService
@@ -22,20 +24,7 @@ class PaymentService:
         total = (
             receipt.discounted_total if receipt.discounted_total is not None else receipt.total
         )
-        url = "https://open.er-api.com/v6/latest/GEL"
-        response = requests.get(url)
-        if response.status_code != 200:
-            raise Exception("Failed to fetch currency rates.")
-
-        data = response.json()
-        rates = data.get("rates", {})
-
-        if currency_id not in rates:
-            raise IndexError(f"Currency {currency_id} is not supported.")
-
-        conversion_rate = rates[currency_id]
-        converted_total = total * conversion_rate
-
+        converted_total = self.converter.convert("GEL", "USD", total)
         return int(round(converted_total))
 
     # TODO: receipt should get closed after registering payment
--- playground/core/services/interfaces/currency_converter_interface.py
+++ playground/core/services/interfaces/currency_converter_interface.py
@@ -2,5 +2,5 @@ from typing import Protocol
 
 
 class ICurrencyConverter(Protocol):
-    def convert(self, from_currency: str, to_currency: str, amount: int) -> float:
+    def convert(self, from_currency: str, to_currency: str, amount: float) -> float:
         pass
